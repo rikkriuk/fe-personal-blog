bddd010a8809799b77f2236bd684de0ca72d6fa1
ikokywu <rikiridwan949@gmail.com>
2024-12-01 14:58:39 +0700

    feat: add web-logo.svg and robots.txt configuration

--- src/components/HeadComponent.jsx
+++ src/components/HeadComponent.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import PropTypes from "prop-types";
+import WebIcon from "../assets/web-icon.svg";
 
 const HeadComponent = ({ title, description, keywords }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <link rel="icon" href={WebIcon} />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {/* <meta property="og:image" content="/default-image.jpg" />
-      <meta property="og:url" content={window.location.href} /> */}
+      <meta property="og:site_name" content="My Blog" />
+      <meta property="og:url" content={window.location.href} />
     </Helmet>
   );
 };
 
-HeadComponent.defaultProps = {
-  title: "My Blog - Discover Amazing Content",
-  description: "Welcome to My Blog, where you can find amazing content about various topics.",
-  keywords: "blog, articles, content, amazing, topics",
-};
 
 HeadComponent.propTypes = {
   title: PropTypes.string,
